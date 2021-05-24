0ededb8bf61e701bc6a62f44523dd1b10d8d72f1
Draylar <samuelhimes@gmail.com>
2021-05-24 14:57:45 -0500

    fix #28

--- src/main/java/draylar/gofish/GoFishClient.java
+++ src/main/java/draylar/gofish/GoFishClient.java
@@ -4,6 +4,7 @@ import draylar.gofish.client.be.AstralCrateRenderer;
 import draylar.gofish.client.item.AstralCrateItemRenderer;
 import draylar.gofish.registry.GoFishBlocks;
 import draylar.gofish.registry.GoFishEntities;
+import draylar.gofish.registry.GoFishItems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.EnvType;
@@ -25,6 +26,16 @@ public class GoFishClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(GoFishBlocks.ASTRAL_CRATE, RenderLayer.getCutout());
         BlockEntityRendererRegistry.INSTANCE.register(GoFishEntities.ASTRAL_CRATE, AstralCrateRenderer::new);
         BuiltinItemRendererRegistry.INSTANCE.register(GoFishBlocks.ASTRAL_CRATE.asItem(), new AstralCrateItemRenderer());
+
+        registerFishingRodPredicates(GoFishItems.BLAZE_ROD);
+        registerFishingRodPredicates(GoFishItems.CELESTIAL_ROD);
+        registerFishingRodPredicates(GoFishItems.FROSTED_ROD);
+        registerFishingRodPredicates(GoFishItems.SOUL_ROD);
+        registerFishingRodPredicates(GoFishItems.MATRIX_ROD);
+        registerFishingRodPredicates(GoFishItems.SLIME_ROD);
+        registerFishingRodPredicates(GoFishItems.DIAMOND_REINFORCED_ROD);
+        registerFishingRodPredicates(GoFishItems.SKELETAL_ROD);
+        registerFishingRodPredicates(GoFishItems.EYE_OF_FISHING);
     }
 
     public void registerFishingRodPredicates(Item item) {
