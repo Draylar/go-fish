9d4d5726c119059ff1ac175e185527914e2939f3
Draylar <17728338+Draylar@users.noreply.github.com>
2021-09-06 19:18:18 -0500

    remove AutoConfig field in main initializer

--- src/main/java/draylar/gofish/GoFish.java
+++ src/main/java/draylar/gofish/GoFish.java
@@ -1,16 +1,12 @@
 package draylar.gofish;
 
 import draylar.gofish.command.FishCommand;
-import draylar.gofish.config.GoFishConfig;
 import draylar.gofish.registry.*;
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.potion.Potions;
 import net.minecraft.recipe.BrewingRecipeRegistry;
 import net.minecraft.util.Identifier;
@@ -20,7 +16,6 @@ import org.apache.logging.log4j.Logger;
 public class GoFish implements ModInitializer {
 
     public static final ItemGroup GROUP = FabricItemGroupBuilder.build(id("group"), () -> new ItemStack(GoFishItems.GOLDEN_FISH));
-    public static final GoFishConfig CONFIG = AutoConfig.register(GoFishConfig.class, GsonConfigSerializer::new).getConfig();
     public static final Logger LOGGER = LogManager.getLogger();
 
     @Override
