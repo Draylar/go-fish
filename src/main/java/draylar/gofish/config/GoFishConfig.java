a3128ce04df492aabb698ffaaba448e05510e5cb
Draylar <17728338+Draylar@users.noreply.github.com>
2021-09-06 19:17:16 -0500

    bundling autoconfig when you don't have a config 😎

--- src/main/java/draylar/gofish/config/GoFishConfig.java
+++
@@ -1,9 +0,0 @@
-package draylar.gofish.config;
-
-import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
-import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
-
-@Config(name = "go-fish")
-public class GoFishConfig implements ConfigData {
-    public boolean disableFishingFarms = true;
-}
